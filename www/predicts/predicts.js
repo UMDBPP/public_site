45a7df090110052abbca8d18a2181cf14f18cf73
Zach <zrb@umd.edu>
2018-09-15 01:24:19 -0400

    added geojsons

--- www/predicts/predicts.js
+++ www/predicts/predicts.js
@@ -1,7 +1,31 @@
-var overlay_layers = {};
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
 
-var main_map = L.map('main_map', {layers: [base_layers['OSM Road']]}).setView([39.656674, -77.934194], 9);
+var class_airspace = new L.GeoJSON.AJAX('../data/class_airspace.geojson', {
+    onEachFeature: function (f, l) {
+        l.bindPopup('<pre>' + JSON.stringify(f.properties, null, ' ').replace(/[\{\}"]/g, '') + '</pre>');
+    },
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+});
 
+var mcdonalds = new L.GeoJSON.AJAX('../data/mcdonalds.geojson', {
+    onEachFeature: function (f, l) {
+        l.bindPopup('<pre>' + JSON.stringify(f.properties, null, ' ').replace(/[\{\}"]/g, '') + '</pre>');
+    }
+});
+
+var overlay_layers = {'Airspace': class_airspace, 'McDonald\'s Locations': mcdonalds};
+
+var main_map = L.map('main_map', {layers: [base_layers['OSM Road'], overlay_layers['Airspace']]}).setView([39.656674, -77.934194], 9);
 var layer_control = L.control.layers(base_layers, overlay_layers, {
     collapsed: false
 }).addTo(main_map);
+
